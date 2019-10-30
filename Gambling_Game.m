45c8b4f347ca074869a3a18790eb3bc44b800433
RakibulIslamRakib <56424436+RakibulIslamRakib@users.noreply.github.com>
2019-10-30 08:02:44 -0700

    Create Gambling_Game.m

---
+++ Gambling_Game.m
@@ -0,0 +1,33 @@
+difference=0;
+heads=0;
+tails=0;
+si=1;
+gmno=1;
+c='';
+rnd=5; 
+disp('Game No   SI No  Random Numb   Head/Tail  C_Heads   C_Tails   Difference');
+while gmno<=5
+    rnd=randi([0,9]);
+    if rnd<5
+        heads=heads+1;
+        c='H';
+    else
+        tails=tails+1;
+        c='T';
+    end
+    difference=abs(heads-tails);
+    fprintf('  %d       %d          %d           %c           %d        %d        %d\n',gmno,si,rnd,c,heads,tails,difference);
+    if difference==3
+        if si<=8
+            fprintf('Win BDT 1000\n');
+        else 
+            fprintf('Lose BDT 1000\n');
+        end
+        heads=0;
+        tails=0;
+        difference=0;
+        si=0;
+        gmno=gmno+1;
+    end
+    si=si+1;
+end
